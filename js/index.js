84e2706e6349fec9b2bd579b48fafd46f74d7ff4
DylanCheetah <dylan_the_cheetah@outlook.com>
2024-07-25 15:29:42 -0400

    Improve JavaScript init code.

--- js/index.js
+++ js/index.js
@@ -2,5 +2,13 @@
  * index.js
  */
 
-// Initialize carousels
-const scenes = new bootstrap.Carousel(document.querySelector("#scenes"));
+document.addEventListener("DOMContentLoaded", () => {
+    // Initialize carousels
+    const headshots = new bootstrap.Carousel(document.querySelector("#headshots"));
+    const halfbodies = new bootstrap.Carousel(document.querySelector("#halfbodies"));
+    const fullbodies = new bootstrap.Carousel(document.querySelector("#fullbodies"));
+    const scenes = new bootstrap.Carousel(document.querySelector("#scenes"));
+    const basicRefs = new bootstrap.Carousel(document.querySelector("#basicRefs"));
+    const detailedRefs = new bootsrap.Carousel(document.querySelector("#detailedRefs"));
+    const traditional = new bootstrap.Carousel(document.querySelector("#traditional"));
+});
